7983e7406472dd5e005399453d0921cf7fadc96e
IvanGodPro24 <vanyanep@gmail.com>
2025-08-06 16:27:14 +0300

    style not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,18 @@
 import { VscBracketError } from "react-icons/vsc";
+import { Link } from "react-router-dom";
 import css from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div className={css.notfound}>
-      <VscBracketError size="100" />
-      <p className={css.title}>
-        The page you&apos;re looking for can&apos;t be found
-      </p>
+    <div className={css.container}>
+      <div className={css.content}>
+        <VscBracketError size={120} className={css.icon} />
+        <h1 className={css.title}>404</h1>
+        <p className={css.text}>The page you're looking for can't be found</p>
+        <Link to="/" className={css.link}>
+          Return to Home
+        </Link>
+      </div>
     </div>
   );
 };
